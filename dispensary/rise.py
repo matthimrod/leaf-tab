70ef6822eabcd6e2ecd5aa4c52381d2e0f612567
Matt Himrod <matt@matthimrod.com>
2026-07-24 20:51:03 -0400

    Added edibles to Rise.

--- dispensary/rise.py
+++ dispensary/rise.py
@@ -1,4 +1,5 @@
 """Rise Dispensary Location class module."""
+
 import logging
 
 import requests
@@ -28,7 +29,7 @@ class VariantLabResults(BaseModel):
 
 
 class VariantSpecialPrice(BaseModel):
-    price: str
+    price: float
     discount_type: str
     discount_price: str | None
     discount_amount: float | None
@@ -55,9 +56,11 @@ class VariantDetails(VariantBase):
     price_gram: float | None
     price_two_gram: float | None
     price_half_gram: float | None
+    price_each: float | None
     special_price_gram: VariantSpecialPrice | None
     special_price_two_gram: VariantSpecialPrice | None
     special_price_half_gram: VariantSpecialPrice | None
+    special_price_each: VariantSpecialPrice | None
 
 
 class RiseProduct(BaseModel):
@@ -79,6 +82,49 @@ class Result(BaseModel):
 class RiseDispensary(Dispensary):
     """Rise Dispensary Location class."""
 
+    def prepare_row(self, item: VariantDetails, weight: str, amount: str | None) -> Product:
+        if not amount:
+            amount = weight
+        special_price = getattr(item, f'special_price_{weight}')
+        sale_price, sale_type = None, None
+        if special_price:
+            sale_price = float(special_price.discount_price)
+            if special_price.discount_type == 'percent':
+                sale_type = f'{special_price.discount_percent}% off'
+            elif special_price.discount_type == 'target_price':
+                sale_type = f'${special_price.discount_price} sale'
+
+        return Product(
+            id=str(item.product_id),
+            brand=item.brand,
+            type=item.kind,
+            subtype=item.brand_subtype,
+            strain=item.name,
+            strain_type=item.category if item.category else '',
+            product_name=f'{item.name} - {item.brand_subtype}',
+            weight=amount,
+            inventory=None,
+            full_price=float(getattr(item, f'price_{weight}')),
+            sale_price=sale_price,
+            sale_type=sale_type,
+            sale_description=item.special_title,
+            cannabinoids={
+                y.compound_name: y.value / 100.0
+                for x in item.lab_results
+                if x.price_id == weight
+                for y in x.lab_results
+                if self.is_cannabinoid(y.compound_name)
+            },
+            terpenes={
+                y.compound_name: y.value / 100
+                for x in item.lab_results
+                if x.price_id == weight
+                for y in x.lab_results
+                if not self.is_cannabinoid(y.compound_name)
+            },
+            notes=item.store_notes or '',
+        )
+
     def __init__(self, location_name: str, store_id: int) -> None:
         """Construct Rise Dispensary object."""
         super().__init__()
@@ -87,14 +133,17 @@ class RiseDispensary(Dispensary):
 
         logger.info('Creating Dispensary')
         with requests.Session() as session:
-
             total_pages = 1
-            inventory_url = self.URLBuilder(netloc='riseheadless-gtiv2.frontastic.live',
-                                            path='/frontastic/action/product/pagination',
-                                            query_items={'refinementList[root_types][]': 'vape',
-                                                         'page': 0,
-                                                         'storeId': store_id,
-                                                         'stateSlug': '/dispensaries/pennsylvania'})
+            inventory_url = self.URLBuilder(
+                netloc='riseheadless-gtiv2.frontastic.live',
+                path='/frontastic/action/product/pagination',
+                query_items={
+                    'refinementList[root_types][]': 'vape',
+                    'page': 0,
+                    'storeId': store_id,
+                    'stateSlug': '/dispensaries/pennsylvania',
+                },
+            )
 
             while inventory_url.query_items['page'] <= total_pages:  # type: ignore[operator]
                 logger.info('Reading inventory page %d', inventory_url.query_items['page'])
@@ -108,42 +157,39 @@ class RiseDispensary(Dispensary):
                     for item_key, item in variant.variants_details.items():
                         logger.info('Processing item %s / %s', item_key, item.name)
 
-                        for weight_name, price_name, special_price_name in \
-                                [('gram', 'price_gram', 'special_price_gram'),
-                                 ('half_gram', 'price_half_gram', 'special_price_half_gram'),
-                                 ('two_gram', 'price_two_gram', 'special_price_two_gram')]:
-                            if getattr(item, price_name):
-                                row = Product(id=str(item.product_id),
-                                              brand=item.brand,
-                                              type=item.kind,
-                                              subtype=item.brand_subtype,
-                                              strain=item.name,
-                                              strain_type=item.category if item.category else "",
-                                              product_name=" - ".join([item.name, item.brand_subtype]),
-                                              weight=weight_name,
-                                              inventory=None,
-                                              full_price=float(getattr(item, price_name)),
-                                              sale_price=None,
-                                              sale_type=None,
-                                              sale_description=item.special_title,
-                                              cannabinoids={y.compound_name: y.value / 100.0
-                                                            for x in item.lab_results if x.price_id == weight_name
-                                                            for y in x.lab_results
-                                                            if self.is_cannabinoid(y.compound_name)},
-                                              terpenes={y.compound_name: y.value / 100
-                                                        for x in item.lab_results if x.price_id == weight_name
-                                                        for y in x.lab_results
-                                                        if not self.is_cannabinoid(y.compound_name)},
-                                              notes=item.store_notes or '')
+                        for weight in ['gram', 'half_gram', 'two_gram']:
+                            if getattr(item, f'price_{weight}'):
+                                self.inventory.append(
+                                    self.prepare_row(item, weight, weight),
+                                )
 
-                                special_price = getattr(item, special_price_name)
-                                if special_price:
-                                    row.sale_price = special_price.discount_price
-                                    if special_price.discount_type == 'percent':
-                                        row.sale_type = f"{special_price.discount_percent}% off"
-                                    elif special_price.discount_type == 'target_price':
-                                        row.sale_type = f"${special_price.discount_price} sale"
+            total_pages = 1
+            inventory_url = self.URLBuilder(
+                netloc='riseheadless-gtiv2.frontastic.live',
+                path='/frontastic/action/product/pagination',
+                query_items={
+                    'refinementList[root_types][]': 'edible',
+                    'page': 0,
+                    'storeId': store_id,
+                    'stateSlug': '/dispensaries/pennsylvania',
+                },
+            )
 
-                                self.inventory.append(row)
+            while inventory_url.query_items['page'] <= total_pages:  # type: ignore[operator]
+                logger.info('Reading inventory page %d', inventory_url.query_items['page'])
+                response = session.get(url=inventory_url.url)
+                result = Result.model_validate_json(response.text)
+
+                total_pages = result.dataSourcePayload.algolia_total_page
+                inventory_url.query_items['page'] += 1  # type: ignore[operator]
+
+                for variant in result.dataSourcePayload.algolia:
+                    for item_key, item in variant.variants_details.items():
+                        logger.info('Processing item %s / %s', item_key, item.name)
+
+                        weight = 'each'
+                        amount = variant.variants[weight].amount
+                        if getattr(item, f'price_{weight}'):
+                            self.inventory.append(self.prepare_row(item, weight, amount))
 
         self.process_dataframe()
